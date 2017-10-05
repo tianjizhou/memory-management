c71dc2db4ffa452782dfebdd8d814e0a4641b828
tianjizhou <tianjiterry@gmail.com>
2017-10-05 17:50:07 -0400

    Update Process.h

--- Process.h
+++ Process.h
@@ -1,1 +1,8 @@
 // FILE: Process.h
+class Process {
+  //I'll make everything public for now...
+  public:
+  //Let's assume there is no I/O burst...
+  int total_CPU_time;
+  int remaining_time;
+}
