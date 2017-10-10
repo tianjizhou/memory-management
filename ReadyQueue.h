0e91b63fdc157beaaec99e576b03b556da07e796
Chocn <32555873+Chocn@users.noreply.github.com>
2017-10-09 22:13:39 -0400

    Add files via upload

---
+++ ReadyQueue.h
@@ -0,0 +1,35 @@
+// FILE: ReadyQueue.h
+#ifndef _readyqueue_h
+#define _readyqueue_h
+
+#include "process.h"
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+
+
+class ReadyQueue {
+public:
+	// CONSTRUCTOR
+	ReadyQueue() { processes_ = NULL;}
+	
+	// ACCESSORS
+	Process* front() { return processes_.front(); }
+	bool empty() const { return processes_.empty(); }
+	int size() const { return processes_.size(); }
+	
+	// MODIFIERS
+	void push(std::vector<Process*> new_processes);
+	void pop() { processes_.pop_front(); }
+	void sort(const std::string& mode);
+
+private:
+	// REPRESENTATION
+	std::list<Process*> processes_;
+	bool FCFS(Process* p1, Process* p2);
+	bool SRT(Process* p1, Process* p2);
+	bool RR(Process* p1, Process* p2);
+};
+
+#endif
