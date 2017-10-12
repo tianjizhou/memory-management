be40525b3d88e9ce4b05c1458345ec63c5344116
tianjizhou <tianjiterry@gmail.com>
2017-10-12 18:46:35 -0400

    Update process.h

--- process.h
+++ process.h
@@ -11,18 +11,17 @@ class Process {
 		// CONSTRUCTOR
         Process( char c , int t_arrival , int t_burst , int c_burst , int t_IO ) {
                  PID_ = c;
-                 arrival_time_ = t_arrival; 
+                 initial_arrival_time_ = t_arrival; 
                  single_CPU_time_ = t_burst;
-                 total_CPU_time_ = t_burst;
-                 total_bursts_ = c_burst; 
+		 total_bursts_ = c_burst; 
                  IO_time_ = t_IO;
         }
 
 		// ACCESSORS
 		char ID() const { return PID_; }
-        int arrival() const { return arrival_time_ ; }
-        int burst_time() const { return single_CPU_time_ ;}
-        int burst_num() const { return remaining_bursts_ ; }
+        	int arrival() const { return arrival_time_ ; }
+        	int burst_time() const { return single_CPU_time_ ;}
+        	int burst_num() const { return remaining_bursts_ ; }
 		int tIO() const { return IO_time_ ; }
 		int tCPU() const { return remaining_time_; }
 		bool single_complete() const { return (remaining_time_ == 0); }
@@ -31,10 +30,11 @@ class Process {
 		// bool all_complete() {}
 
 		// MODIFIERS
+		void reset() {remaining_bursts_=total_bursts_;remaining_time_=single_CPU_time_;remaining_IO_time_=IO_time_;arrival_time_=initial_arrival_time_;}
 		void CPU_tick() { if (remaining_time_>0) remaining_time_--; }
 		void IO_tick() { if (remaining_IO_time_>0) remaining_IO_time_--; }
 		void IO_reset() { remaining_IO_time_=IO_time_; }
-        void decrease_bursts() { if ( remaining_bursts_ > 0 ) remaining_bursts_--; }
+        	void decrease_bursts() { if ( remaining_bursts_ > 0 ) remaining_bursts_--; }
 
 	private:
 
@@ -42,9 +42,8 @@ class Process {
 		// CONSTANT
 		char PID_;              // process id
 		int single_CPU_time_;   // CPU burst time
-		int IO_time_;
-       		int arrival_time_;      // arrival time
-  		int total_CPU_time_;
+		int IO_time_;		// IO time
+       		int initial_arrival_time_;      // arrival time
 		int total_bursts_;
 
 
@@ -52,7 +51,7 @@ class Process {
 	    	int remaining_bursts_;  // number of CPU bursts	
   		int remaining_time_;	// remaining time in single burst
 		int remaining_IO_time_; // remaining time of current I/O blocking
-
+		int arrival_time_;
 };
 
 #endif
